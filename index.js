efc4c321b17c34384a9a22f5ecc3e87f3c9ae3c2
eddah chepkoech <eddahsjr@gmail.com>
2024-06-25 14:38:48 +0300

    condition ifelse,switchcasedefaultand tenerary

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function scuberGreetingForFeet(scuberLength ){
   
   let result;
-  // if (scuberLength < 0){
-  //   result = "Length cannot be less than 0.";
-  // }
+  
 
   if (scuberLength <= 400) {
     result = "This one is on me!";
@@ -14,9 +12,7 @@ function scuberGreetingForFeet(scuberLength ){
   else if (scuberLength > 2000 && scuberLength <= 2500){
     result = 'I will gladly take your thirty bucks.';
   }
-  // else if(scuberLength > 2500){
-  //   result = 'No can do.';
-  // }
+ 
   else{
     result = 'No can do.';
   }
@@ -25,31 +21,40 @@ function scuberGreetingForFeet(scuberLength ){
 
  
 function ternaryCheckCity(cityName){
-  let result;
-  if (cityName === "NYC") {
-    result = "Ok, sounds good.";
-  }
-  else {
-    result = "No go.";
-  }
-  return result;
+  return cityName ==="NYC" ? "Ok, sounds good." : "No go.";
+  // let result;
+  // if (cityName === "NYC") {
+  //   result = "Ok, sounds good.";
+  // }
+  // else {
+  //   result = "No go.";
+  // }
+  // return result;
   
 }
 
 function switchOnCharmFromTip(charmFromTip){
-  let result;
-  if (charmFromTip === "generous") {
-    result = "Thank you so much.";
+  switch (charmFromTip){
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
   }
-  else if (charmFromTip === "not as generous"){
-    result = "Thank you.";
-  }
-  else if (charmFromTip === "thanks for everything"){
-    result = "Bye.";
-  }
-  else {
-    result = "Bye.";
-  }
-  return result;
+  // let result;
+  // if (charmFromTip === "generous") {
+  //   result = "Thank you so much.";
+  // }
+  // else if (charmFromTip === "not as generous"){
+  //   result = "Thank you.";
+  // }
+  // else if (charmFromTip === "thanks for everything"){
+  //   result = "Bye.";
+  // }
+  // else {
+  //   result = "Bye.";
+  // }
+  // return result;
  
 }
